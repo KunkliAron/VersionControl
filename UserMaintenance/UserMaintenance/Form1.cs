08b51b167126d6172ef9d0742d8b892767e428f2
Áron Gábor Kunkli <aron.kunkli@stud.uni-corvinus.hu>
2019-12-01 13:02:51 +0100

    asd2

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -12,6 +12,8 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +21,20 @@ namespace UserMaintenance
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = label1.Text,
+                FirstName = label2.Text
+            };
+            users.Add(u);
         }
     }
 }
