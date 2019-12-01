b74a69b7722383ab5433a1afe049c317bcf9a86b
Áron Gábor Kunkli <aron.kunkli@stud.uni-corvinus.hu>
2019-12-01 13:42:46 +0100

    asd6

--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -70,6 +70,15 @@ namespace UserMaintenance {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Torles.
+        /// </summary>
+        internal static string Delete {
+            get {
+                return ResourceManager.GetString("Delete", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Teljes nev.
         /// </summary>
         internal static string FullName {
